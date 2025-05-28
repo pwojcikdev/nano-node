84b0e64a9ab796e641194cfce05e843acf7c2615
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2025-05-28 15:41:54 +0200

    Optimize `message_queue`

--- nano/node/transport/message_queue.hpp
+++ nano/node/transport/message_queue.hpp
@@ -6,6 +6,8 @@
 #include <nano/lib/numbers.hpp>
 #include <nano/node/transport/traffic_type.hpp>
 
+#include <boost/container/static_vector.hpp>
+
 #include <deque>
 #include <functional>
 #include <utility>
@@ -15,22 +17,21 @@ namespace nano::transport
 class message_queue final
 {
 public:
-	using callback_t = std::function<void (boost::system::error_code const &, std::size_t)>;
-	using entry_t = std::pair<nano::shared_const_buffer, callback_t>;
-	using value_t = std::pair<traffic_type, entry_t>;
-	using batch_t = std::deque<value_t>;
-	using queue_t = std::pair<traffic_type, std::deque<entry_t>>;
-
 	constexpr static size_t max_size = 32;
 	constexpr static size_t full_size = 4 * max_size;
 	constexpr static size_t batch_size = 8;
 
-	message_queue ()
+	using callback_t = std::function<void (boost::system::error_code const &, std::size_t)>;
+	using entry_t = std::pair<nano::shared_const_buffer, callback_t>;
+	using value_t = std::pair<nano::transport::traffic_type, entry_t>;
+	using batch_t = boost::container::static_vector<value_t, batch_size>;
+
+	message_queue () noexcept
 	{
 		clear ();
 	}
 
-	void clear ()
+	void clear () noexcept
 	{
 		for (auto type : all_traffic_types ())
 		{
@@ -38,46 +39,44 @@ public:
 		}
 		current = queues.end ();
 		counter = 0;
+		total_size = 0;
 	}
 
-	bool empty () const
+	bool empty () const noexcept
 	{
-		return std::all_of (queues.begin (), queues.end (), [] (auto const & queue) {
-			return queue.second.empty ();
-		});
+		return total_size == 0;
 	}
 
-	size_t size () const
+	size_t size () const noexcept
 	{
-		return std::accumulate (queues.begin (), queues.end (), size_t{ 0 }, [] (size_t acc, auto const & queue) {
-			return acc + queue.second.size ();
-		});
+		return total_size;
 	}
 
-	size_t size (traffic_type type) const
+	size_t size (traffic_type type) const noexcept
 	{
 		return queues.at (type).second.size ();
 	}
 
-	bool max (traffic_type type) const
+	bool max (traffic_type type) const noexcept
 	{
 		return size (type) >= max_size;
 	}
 
-	bool full (traffic_type type) const
+	bool full (traffic_type type) const noexcept
 	{
 		return size (type) >= full_size;
 	}
 
-	void push (traffic_type type, entry_t entry)
+	void push (traffic_type type, entry_t entry) noexcept
 	{
 		debug_assert (!full (type)); // Should be checked before calling this function
-		queues.at (type).second.push_back (entry);
+		queues.at (type).second.push_back (std::move (entry));
+		++total_size;
 	}
 
-	value_t next ()
+	value_t next () noexcept
 	{
-		debug_assert (!empty ());
+		debug_assert (!empty ()); // Should be checked before calling next
 
 		auto should_seek = [&, this] () {
 			if (current == queues.end () || current->second.empty () || counter >= priority (current->first))
@@ -85,6 +84,22 @@ public:
 				return true;
 			}
 			return false;
+		};
+
+		auto seek_next = [&, this] () {
+			counter = 0;
+			do
+			{
+				if (current != queues.end ())
+				{
+					++current;
+				}
+				if (current == queues.end ())
+				{
+					current = queues.begin ();
+				}
+				release_assert (current != queues.end ());
+			} while (current->second.empty ());
 		};
 
 		if (should_seek ())
@@ -98,16 +113,18 @@ public:
 		auto & queue = current->second;
 
 		++counter;
+		--total_size;
+
 		release_assert (!queue.empty ());
-		auto entry = queue.front ();
+		auto entry = std::move (queue.front ());
 		queue.pop_front ();
-		return { source, entry };
+		return { source, std::move (entry) };
 	}
 
-	batch_t next_batch (size_t max_count = batch_size)
+	batch_t next_batch () noexcept
 	{
-		std::deque<value_t> result;
-		while (!empty () && result.size () < max_count)
+		batch_t result;
+		while (!empty () && result.size () < batch_size)
 		{
 			result.emplace_back (next ());
 		}
@@ -115,24 +132,7 @@ public:
 	}
 
 private:
-	void seek_next ()
-	{
-		counter = 0;
-		do
-		{
-			if (current != queues.end ())
-			{
-				++current;
-			}
-			if (current == queues.end ())
-			{
-				current = queues.begin ();
-			}
-			release_assert (current != queues.end ());
-		} while (current->second.empty ());
-	}
-
-	size_t priority (traffic_type type) const
+	static size_t constexpr priority (nano::transport::traffic_type type) noexcept
 	{
 		switch (type)
 		{
@@ -144,8 +144,12 @@ private:
 		}
 	}
 
-	nano::enum_array<traffic_type, queue_t> queues{};
-	nano::enum_array<traffic_type, queue_t>::iterator current{ queues.end () };
+	using queue_t = std::pair<nano::transport::traffic_type, std::deque<entry_t>>;
+	using queues_t = nano::enum_array<nano::transport::traffic_type, queue_t>;
+	queues_t queues{};
+	queues_t::iterator current{ queues.end () };
+
 	size_t counter{ 0 };
+	size_t total_size{ 0 };
 };
 }
