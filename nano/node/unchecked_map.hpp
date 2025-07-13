bf89b8840366599cf39ff07f74a308bd84a2f71e
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2025-07-13 16:07:21 +0200

    Unchecked map support eventual consistency

--- nano/node/unchecked_map.hpp
+++ nano/node/unchecked_map.hpp
@@ -2,9 +2,12 @@
 
 #include <nano/lib/locks.hpp>
 #include <nano/lib/numbers.hpp>
+#include <nano/lib/numbers_templ.hpp>
 #include <nano/lib/observer_set.hpp>
+#include <nano/node/fwd.hpp>
 #include <nano/secure/common.hpp>
 
+#include <boost/multi_index/hashed_index.hpp>
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/ordered_index.hpp>
 #include <boost/multi_index/random_access_index.hpp>
@@ -17,35 +20,33 @@ namespace mi = boost::multi_index;
 
 namespace nano
 {
-class stats;
-
 class unchecked_map
 {
+	static constexpr size_t max_triggered_entries{ 1000000 }; // 1M entries
+
 public:
-	unchecked_map (unsigned const max_unchecked_blocks, nano::stats &, bool const & do_delete);
+	unchecked_map (unsigned max_size, nano::stats &, bool disable_delete);
 	~unchecked_map ();
 
 	void start ();
 	void stop ();
 
 	void put (nano::hash_or_account const & dependency, nano::unchecked_info const & info);
+	void trigger (nano::hash_or_account const & dependency);
+
+	bool exists (nano::unchecked_key const & key) const;
+	void del (nano::unchecked_key const & key);
+	void clear ();
+
 	void for_each (
 	std::function<void (nano::unchecked_key const &, nano::unchecked_info const &)> action, std::function<bool ()> predicate = [] () { return true; });
 	void for_each (
 	nano::hash_or_account const & dependency, std::function<void (nano::unchecked_key const &, nano::unchecked_info const &)> action, std::function<bool ()> predicate = [] () { return true; });
 	std::vector<nano::unchecked_info> get (nano::block_hash const &);
-	bool exists (nano::unchecked_key const & key) const;
-	void del (nano::unchecked_key const & key);
-	void clear ();
-
-	/**
-	 * Trigger requested dependencies
-	 */
-	void trigger (nano::hash_or_account const & dependency);
 
 	size_t count () const; // Same as `entries_size ()`
 	size_t entries_size () const;
-	size_t queries_size () const;
+	size_t triggered_size () const;
 
 	nano::container_info container_info () const;
 
@@ -54,25 +55,21 @@ public: // Events
 
 private:
 	void run ();
-	void query_impl (nano::block_hash const & hash);
+	void trigger_impl (nano::block_hash const & hash);
 
 private: // Dependencies
 	nano::stats & stats;
 
 private:
-	bool const & disable_delete;
-	std::deque<nano::hash_or_account> buffer;
-	std::deque<nano::hash_or_account> back_buffer;
-	bool writing_back_buffer{ false };
+	unsigned const max_size;
+	bool const disable_delete;
+
+	std::deque<nano::hash_or_account> triggered;
 
 	bool stopped{ false };
 	nano::condition_variable condition;
-	mutable nano::mutex mutex; // Protects queries
+	mutable std::recursive_mutex mutex;
 	std::thread thread;
-
-	unsigned const max_unchecked_blocks;
-
-	void process_queries (decltype (buffer) const & back_buffer);
 
 private:
 	struct entry
@@ -84,15 +81,34 @@ private:
 	// clang-format off
 	class tag_sequenced {};
 	class tag_root {};
+	class tag_hash {};
 
 	using ordered_unchecked = boost::multi_index_container<entry,
-		mi::indexed_by<
-			mi::sequenced<mi::tag<tag_sequenced>>,
-			mi::ordered_unique<mi::tag<tag_root>,
-				mi::member<entry, nano::unchecked_key, &entry::key>>>>;
+	mi::indexed_by<
+		mi::sequenced<mi::tag<tag_sequenced>>,
+		mi::ordered_unique<mi::tag<tag_root>,
+			mi::member<entry, nano::unchecked_key, &entry::key>>
+	>>;
 	// clang-format on
+
 	ordered_unchecked entries;
 
-	mutable std::recursive_mutex entries_mutex; // Protects entries
+private:
+	// Cache for recently triggered hashes to handle race conditions
+	struct triggered_entry
+	{
+		nano::block_hash hash;
+	};
+
+	// clang-format off
+	using triggered_cache = boost::multi_index_container<triggered_entry,
+	mi::indexed_by<
+		mi::sequenced<mi::tag<tag_sequenced>>,
+		mi::hashed_unique<mi::tag<tag_hash>,
+			mi::member<triggered_entry, nano::block_hash, &triggered_entry::hash>>
+	>>;
+	// clang-format on
+
+	triggered_cache recently_triggered;
 };
 }
