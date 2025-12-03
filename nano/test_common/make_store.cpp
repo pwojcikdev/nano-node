304bacfc2717e7cb43ade12de84e002d0d91e00f
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2025-12-03 21:19:12 +0100

    TEST LEDGER UPGRADES

--- nano/test_common/make_store.cpp
+++ nano/test_common/make_store.cpp
@@ -3,12 +3,38 @@
 #include <nano/lib/stats.hpp>
 #include <nano/node/make_store.hpp>
 #include <nano/secure/common.hpp>
+#include <nano/store/lmdb/backend_lmdb.hpp>
+#include <nano/store/rocksdb/backend_rocksdb.hpp>
 #include <nano/store/store.hpp>
+#include <nano/test_common/common.hpp>
 #include <nano/test_common/make_store.hpp>
 
-std::unique_ptr<nano::store::ledger_store> nano::test::make_store ()
+std::unique_ptr<nano::store::backend> nano::test::make_backend (std::filesystem::path path)
 {
-	// Create a simple stats instance for testing
-	static nano::stats test_stats{ nano::default_logger () };
-	return nano::make_store (nano::default_logger (), test_stats, nano::unique_path (), nano::dev::constants);
+	path = path.empty () ? nano::unique_path () : path;
+
+	auto backend_type = nano::default_database_backend ();
+	switch (backend_type)
+	{
+		case nano::database_backend::lmdb:
+		{
+			auto db_path = path / "data.ldb";
+			nano::lmdb_config lmdb_config{};
+			return std::make_unique<nano::store::lmdb::backend_lmdb> (db_path, nano::test::default_logger (), lmdb_config);
+		}
+		case nano::database_backend::rocksdb:
+		{
+			auto db_path = path / "rocksdb";
+			nano::rocksdb_config rocksdb_config{};
+			return std::make_unique<nano::store::rocksdb::backend_rocksdb> (db_path, rocksdb_config);
+		}
+	}
+	release_assert (false, "unknown database backend");
+}
+
+std::unique_ptr<nano::store::ledger_store> nano::test::make_store (std::filesystem::path path)
+{
+	path = path.empty () ? nano::unique_path () : path;
+
+	return nano::make_store (nano::test::default_logger (), nano::test::default_stats (), path, nano::dev::constants);
 }
--- nano/test_common/make_store.hpp
+++ nano/test_common/make_store.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
+#include <nano/store/fwd.hpp>
+
+#include <filesystem>
 #include <memory>
 
-namespace nano::store
-{
-class ledger_store;
-}
-
+/**
+ * Helper functions to create a backend directly for testing
+ */
 namespace nano::test
 {
-std::unique_ptr<nano::store::ledger_store> make_store ();
+std::unique_ptr<nano::store::backend> make_backend (std::filesystem::path path = {});
+std::unique_ptr<nano::store::ledger_store> make_store (std::filesystem::path path = {});
 }
