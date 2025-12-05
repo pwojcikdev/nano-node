a5d44e75a07292cf29277185329b0aca1f7d10bd
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2025-12-05 22:48:46 +0100

    MIGRATE WIP

--- nano/node/make_store.hpp
+++ nano/node/make_store.hpp
@@ -5,6 +5,9 @@
 
 namespace nano
 {
+// Returns the database path for the given backend type
+std::filesystem::path database_path_for_backend (std::filesystem::path const & base_path, database_backend backend_type);
+
 std::unique_ptr<nano::store::ledger_store> make_store (
 nano::logger &,
 nano::stats &,
