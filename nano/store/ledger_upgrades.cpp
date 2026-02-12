e04a8b9aaede24c749c699e02016e18aa3dd2422
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2026-02-12 19:00:24 +0100

    UPGRADE WIP

--- nano/store/ledger_upgrades.cpp
+++ nano/store/ledger_upgrades.cpp
@@ -1,8 +1,36 @@
+#include <nano/lib/bounded_dfs.hpp>
+#include <nano/store/ledger/account.hpp>
+#include <nano/store/ledger/block.hpp>
+#include <nano/store/ledger/confirmation_height.hpp>
+#include <nano/store/ledger/final_vote.hpp>
+#include <nano/store/ledger/online_weight.hpp>
+#include <nano/store/ledger/peer.hpp>
+#include <nano/store/ledger/pending.hpp>
+#include <nano/store/ledger/pruned.hpp>
+#include <nano/store/ledger/rep_weight.hpp>
+#include <nano/store/ledger/topology.hpp>
+#include <nano/store/ledger/version.hpp>
 #include <nano/store/ledger_store.hpp>
 
 namespace nano::store
 {
+// Builds a topological ordering index for every block in the ledger
+// Each block is assigned an index that is strictly greater than all of its dependencies,
+// The index is stored both in each block's sideband data and in a new `topology` table
 void ledger_store::upgrade_v24_to_v25 ()
 {
+	logger.info (nano::log::type::ledger_upgrade, "Upgrading database from v24 to v25...");
+
+	backend.open (schema_v25, nano::store::open_mode::read_write);
+	{
+		release_assert (backend.get_version (backend.tx_begin_read ()) == 24, "unexpected version during upgrade", std::to_string (backend.get_version (backend.tx_begin_read ())));
+		topology.clear ();
+
+		// logger.info (nano::log::type::ledger_upgrade, "Done processing {} blocks", processed);
+		// version.put (transaction, 25);
+	}
+	backend.close ();
+
+	logger.info (nano::log::type::ledger_upgrade, "Upgrading database from v24 to v25 completed");
 }
 }
