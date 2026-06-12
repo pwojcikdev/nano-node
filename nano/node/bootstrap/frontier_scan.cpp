184128ad54d1c99262ad6f137102425d0de75d71
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2026-06-12 20:54:49 +0200

    Extract frontier scan policy helpers

---
+++ nano/node/bootstrap/frontier_scan.cpp
@@ -0,0 +1,5 @@
+#include <nano/node/bootstrap/frontier_scan.hpp>
+
+namespace nano::bootstrap
+{
+}
---
+++ nano/node/bootstrap/frontier_scan.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <nano/lib/numbers.hpp>
+#include <nano/secure/fwd.hpp>
+
+#include <cstddef>
+#include <deque>
+#include <utility>
+
+namespace nano::bootstrap
+{
+}
